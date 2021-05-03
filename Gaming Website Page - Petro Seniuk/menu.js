9f38e4f258b5c622e39db5c0d79170ec524c7066
Petro2006 <74017446+Petro2006@users.noreply.github.com>
2021-05-03 20:19:11 +0300

    Gaming Website Page - Petro Seniuk
      <p>Hello World! I understand HTML, CSS and JS!</p>

---
+++ Gaming Website Page - Petro Seniuk/menu.js
@@ -0,0 +1,13 @@
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu(){
+	if(menuList.style.maxheight == "0px")
+	{
+		menuLlist.style.maxHeight = "130px";
+	}
+	else{
+			menuList.style.maxHeight = "0px";
+	}
+}
